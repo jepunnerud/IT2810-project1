023b9172d9bf91be12e51f6be4495f4f0381c1d3
jepunnerud <jepunnerud@gmail.com>
2023-09-13 12:47:29 +0200

    GL-15: setup ci/cd pipelines

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,56 +1,44 @@
-import React, { useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react'
 
 export default function InfoPage() {
-
   //Satt til NOR, bør settes til cca3 til landet man har klikket på
-  const countryCode="NOR"
-  const [isFavourite, setIsFavourite] = useState(false);
-  const [message, setMessage] = useState("");
+  const countryCode = 'NOR'
+  const [isFavourite, setIsFavourite] = useState(false)
+  const [message, setMessage] = useState('')
 
-
-  const storedFavourites = JSON.parse(localStorage.getItem('favourites') ||'[]')
+  const storedFavourites = JSON.parse(
+    localStorage.getItem('favourites') || '[]'
+  )
 
   useEffect(() => {
-    setIsFavourite(storedFavourites.includes(countryCode));
+    setIsFavourite(storedFavourites.includes(countryCode))
 
     if (storedFavourites.includes(countryCode)) {
-      setMessage("Fjern fra favoritter");
-
+      setMessage('Fjern fra favoritter')
+    } else {
+      setMessage('Legg til favoritt')
     }
-    else{
-      setMessage("Legg til favoritt");
-    } 
+  }, [storedFavourites])
 
-  }, [countryCode]);
-  
-  
-  
-  function handleOnClick(){
-
-      if (!isFavourite) {
-        storedFavourites.push(countryCode);
-        localStorage.setItem('favourites', JSON.stringify(storedFavourites));
-        setIsFavourite(true);
-        setMessage("Fjern fra favoritter");
-      }
-      else{
-        const newList: string[] = storedFavourites.filter((code: string) => code !== countryCode);
-        localStorage.setItem('favourites', JSON.stringify(newList));
-        setIsFavourite(false);
-        setMessage("Legg til favoritt");
-
-      }
+  function handleOnClick() {
+    if (!isFavourite) {
+      storedFavourites.push(countryCode)
+      localStorage.setItem('favourites', JSON.stringify(storedFavourites))
+      setIsFavourite(true)
+      setMessage('Fjern fra favoritter')
+    } else {
+      const newList: string[] = storedFavourites.filter(
+        (code: string) => code !== countryCode
+      )
+      localStorage.setItem('favourites', JSON.stringify(newList))
+      setIsFavourite(false)
+      setMessage('Legg til favoritt')
+    }
   }
-  return(
+  return (
     <>
-        <h1>Info</h1>
-        {
-          <button onClick={handleOnClick}>
-            {message}
-          </button>
-        }
+      <h1>Info</h1>
+      {<button onClick={handleOnClick}>{message}</button>}
     </>
-  ) 
+  )
 }
-
