9d0289b3118bcf96038059fd311bcf50e5b39ef6
emma <emmagjo@ntnu.no>
2023-09-21 09:36:40 +0200

    GL-26: finished home page render test

--- src/pages/test/Home.test.tsx
+++ src/pages/test/Home.test.tsx
@@ -1,19 +1,17 @@
-import ReactDOM, { render } from "react-dom";
+import { render, unmountComponentAtNode } from "react-dom";
 import HomePage from "../Home";
 import { expect, it, test } from 'vitest'
 
 test('Home Page ', () => {
     it('renders the search input and sorting dropdown', () => {
-        const container = document.createElement('div'); // Create a container element
-        document.body.appendChild(container); // Append it to the document
-
+        const container = document.createElement('div');
+        document.body.appendChild(container);
         render(<HomePage />, container);
 
         expect(container.querySelector('.input')).not.toBeNull();
         expect(container.querySelector('#sorting-parameter')).not.toBeNull();
 
-        // Clean up after the test
-        ReactDOM.unmountComponentAtNode(container);
+        unmountComponentAtNode(container);
         document.body.removeChild(container);
     });
 })
