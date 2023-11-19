ca8eded9a92062a14703bd78345094a3c084c067
jepunnerud <jepunnerud@gmail.com>
2023-11-19 17:13:36 +0100

    Optimize responsive design

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,15 +1,16 @@
+import { Link } from 'react-router-dom'
 import { Country } from '../types'
 import './CountryCard.css'
 
 function CountryCard(props: { country: Country }) {
   return (
     <>
-      <a href={'/project1/info/' + props.country.cca3}>
+      <Link to={'/info/' + props.country.cca3}>
         <div data-testid="country-card" className="card">
           <img src={props.country.flags.png} alt={props.country.name.common} />
           <p>{props.country.name.common}</p>
         </div>
-      </a>
+      </Link>
     </>
   )
 }
