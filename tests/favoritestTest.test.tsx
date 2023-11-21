bc3b1d0a6cd43219a1e58f89bc6d2cf45b69bc44
larshfle <larshfle@stud.ntnu.no>
2023-11-21 19:03:43 +0100

    prettier

--- tests/favoritestTest.test.tsx
+++ tests/favoritestTest.test.tsx
@@ -6,43 +6,43 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { setFavourite } from '../src/hooks/Countries.ts'
 
 beforeAll(() => {
-    server.listen()
+  server.listen()
 })
 
 afterAll(() => {
-    server.close()
+  server.close()
 })
 
 const queryClient = new QueryClient()
 
 describe('FavouritesPage', () => {
-    const renderFavouritesPage = () => {
-        render(
-            <QueryClientProvider client={queryClient}>
-                <FavouritesPage />
-            </QueryClientProvider>
-        )
-    }
+  const renderFavouritesPage = () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <FavouritesPage />
+      </QueryClientProvider>
+    )
+  }
 
-    it('renders no card-holder components when localStorage is empty', async () => {
-        renderFavouritesPage()
-        //Clears localstorage so no country-cards shold be loaded
-        localStorage.clear()
-        const favouriteCountries = screen.queryAllByTestId('country-card')
+  it('renders no card-holder components when localStorage is empty', async () => {
+    renderFavouritesPage()
+    //Clears localstorage so no country-cards shold be loaded
+    localStorage.clear()
+    const favouriteCountries = screen.queryAllByTestId('country-card')
 
-        //Checks that no favourite cards are loaded
-        expect(favouriteCountries).toHaveLength(0)
-    })
+    //Checks that no favourite cards are loaded
+    expect(favouriteCountries).toHaveLength(0)
+  })
 
-    it('renders the list of favorite countries when isLoading is false', async () => {
-        renderFavouritesPage()
-        //Sets a favourite, which adds a code in localstorage
-        setFavourite('CAN')
+  it('renders the list of favorite countries when isLoading is false', async () => {
+    renderFavouritesPage()
+    //Sets a favourite, which adds a code in localstorage
+    setFavourite('CAN')
 
-        //await screen.findByText('Canada')
+    //await screen.findByText('Canada')
 
-        //Checks that the country is loaded on the favorites page
-        const country = screen.findByText('Canada')
-        expect(country).toBeDefined()
-    })
+    //Checks that the country is loaded on the favorites page
+    const country = screen.findByText('Canada')
+    expect(country).toBeDefined()
+  })
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        include: ['**/*.test.tsx'],
-        globals: true,
-        environment: 'jsdom',
-    },
+  plugins: [react()],
+  test: {
+    include: ['**/*.test.tsx'],
+    globals: true,
+    environment: 'jsdom',
+  },
 })
